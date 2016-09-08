bc3a70c7b5868f76e8031ded3c7dc6313e9f1716
夜云 <644362887@qq.com>
2016-09-09 00:50:41 +0800

    更正错误

--- views/home/grouping/_list.php
+++ views/home/grouping/_list.php
@@ -64,46 +64,47 @@
         <tr>
             <td class="text-center"><?= $m->qishu ?></td>
 
-            <?php if($unit != 1): ?>
-                <?php echo $m->one ?>
-            <?php else : ?>
-                <span class="badge bg-gray" style="background: red">
-                        <?php echo $m->one ?>
-                    </span>
-            <?php endif;?>
+            <td class="text-center">
+                <?php if($unit != 1): ?>
+                    <?php echo $m->one ?>
+                <?php else : ?>
+                    <span class="badge bg-gray" style="background: red">
+                            <?php echo $m->one ?>
+                        </span>
+                <?php endif;?>
 
-            <?php if($unit != 2): ?>
-                <?php echo $m->two ?>
-            <?php else : ?>
-                <span class="badge bg-gray" style="background: red">
-                        <?php echo $m->two ?>
-                    </span>
-            <?php endif;?>
+                <?php if($unit != 2): ?>
+                    <?php echo $m->two ?>
+                <?php else : ?>
+                    <span class="badge bg-gray" style="background: red">
+                            <?php echo $m->two ?>
+                        </span>
+                <?php endif;?>
 
-            <?php if($unit != 3): ?>
-                <?php echo $m->three ?>
-            <?php else : ?>
-                <span class="badge bg-gray" style="background: red">
-                        <?php echo $m->three ?>
-                    </span>
-            <?php endif;?>
+                <?php if($unit != 3): ?>
+                    <?php echo $m->three ?>
+                <?php else : ?>
+                    <span class="badge bg-gray" style="background: red">
+                            <?php echo $m->three ?>
+                        </span>
+                <?php endif;?>
 
-            <?php if($unit != 4): ?>
-                <?php echo $m->four ?>
-            <?php else : ?>
-                <span class="badge bg-gray" style="background: red">
-                        <?php echo $m->four ?>
-                    </span>
-            <?php endif;?>
+                <?php if($unit != 4): ?>
+                    <?php echo $m->four ?>
+                <?php else : ?>
+                    <span class="badge bg-gray" style="background: red">
+                            <?php echo $m->four ?>
+                        </span>
+                <?php endif;?>
 
-            <?php if($unit != 5): ?>
-                <?php echo $m->five ?>
-            <?php else : ?>
-                <span class="badge bg-gray" style="background: red">
-                        <?php echo $m->five ?>
-                    </span>
-            <?php endif;?>
-
+                <?php if($unit != 5): ?>
+                    <?php echo $m->five ?>
+                <?php else : ?>
+                    <span class="badge bg-gray" style="background: red">
+                            <?php echo $m->five ?>
+                        </span>
+                <?php endif;?>
+            </td>
 
             <td class="text-center"><span class="badge bg-gray" <?php echo !empty($m->analysisXjsscs->front_three_lucky_txt) ? 'style="background: red"' : false;  ?> >  <?php echo !empty($m->analysisXjsscs->front_three_lucky_txt) ? '中' : '未';  ?> </span></td>
             <td class="text-center"><span class="badge bg-gray" <?php echo !empty($m->analysisXjsscs->center_three_lucky_txt) ? 'style="background: red"' : false;  ?>> <?php echo !empty($m->analysisXjsscs->center_three_lucky_txt) ? '中' : '未';  ?> </span></td>
