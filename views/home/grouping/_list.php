58870c91289c70ded9e6ab45b7b306f94a0d7093
夜云 <644362887@qq.com>
2016-09-09 20:24:20 +0800

    收工了

--- views/home/grouping/_list.php
+++ views/home/grouping/_list.php
@@ -48,9 +48,9 @@
 
 
             </td>
-            <td class="text-center"><span class="badge bg-gray" <?php echo !empty($m->analysisolds->front_three_lucky_txt) ? 'style="background: red"' : false;  ?> >  <?php echo !empty($m->analysisTjsscs->front_three_lucky_txt) ? '中' : '未';  ?> </span></td>
-            <td class="text-center"><span class="badge bg-gray" <?php echo !empty($m->analysisolds->center_three_lucky_txt) ? 'style="background: red"' : false;  ?>> <?php echo !empty($m->analysisTjsscs->center_three_lucky_txt) ? '中' : '未';  ?> </span></td>
-            <td class="text-center"><span class="badge bg-gray" <?php echo !empty($m->analysisolds->after_three_lucky_txt) ? 'style="background: red"' : false;  ?>> <?php echo !empty($m->analysisTjsscs->after_three_lucky_txt) ? '中' : '未';  ?> </span></td>
+            <td class="text-center"><span class="badge bg-gray" <?php echo !empty($m->analysisolds->front_three_lucky_txt) ? 'style="background: red"' : false;  ?> >  <?php echo !empty($m->analysisolds->front_three_lucky_txt) ? '中' : '未';  ?> </span></td>
+            <td class="text-center"><span class="badge bg-gray" <?php echo !empty($m->analysisolds->center_three_lucky_txt) ? 'style="background: red"' : false;  ?>> <?php echo !empty($m->analysisolds->center_three_lucky_txt) ? '中' : '未';  ?> </span></td>
+            <td class="text-center"><span class="badge bg-gray" <?php echo !empty($m->analysisolds->after_three_lucky_txt) ? 'style="background: red"' : false;  ?>> <?php echo !empty($m->analysisolds->after_three_lucky_txt) ? '中' : '未';  ?> </span></td>
         </tr>
     <?php endforeach; ?>
 
