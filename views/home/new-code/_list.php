4eb54bc09fb910ef73a8dc4c3927fb43a8c66fab
王海波 <644362887@qq.com>
2017-06-20 23:28:03 +0800

    bug

--- views/home/new-code/_list.php
+++ views/home/new-code/_list.php
@@ -4,7 +4,7 @@
         <td class="text-center"><?= $m->qihao ?></td>
         <td class="text-center"><?php echo $code = str_replace(" ", '', $m->one.$m->two.$m->three.$m->four.$m->five); ?></td>
         <td class="text-center">
-            <span class="badge bg-gray" <?php echo $analysis->attributes['lucky'] == 1 ? 'style="background: red"' : false;  ?> >  <?php echo $analysis->attributes['lucky'] == 1 ? '中' : '未';  ?> </span>
+            <span class="badge bg-gray" <?php echo $analysis->lucky == 1 ? 'style="background: red"' : false;  ?> >  <?php echo $analysis->lucky == 1 ? '中' : '未';  ?> </span>
         </td>
     </tr>
 <?php endforeach; ?>
