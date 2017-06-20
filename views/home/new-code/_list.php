fca3b9d5631b0528a5610ad0c31d2096d0597013
王海波 <644362887@qq.com>
2017-06-20 22:13:02 +0800

    bug

--- views/home/new-code/_list.php
+++ views/home/new-code/_list.php
@@ -1,17 +1,10 @@
 <?php foreach ($model as $m) : ?>
     <?php $analysis = $m->getAnalysis($package_id)->one(); ?>
-
-    <?php
-
-        echo '<pre>';
-        var_dump($analysis->attributes->lucky);
-        var_dump($analysis);exit;
-    ?>
     <tr>
         <td class="text-center"><?= $m->qihao ?></td>
         <td class="text-center"><?php echo $code = str_replace(" ", '', $m->one.$m->two.$m->three.$m->four.$m->five); ?></td>
         <td class="text-center">
-            <span class="badge bg-gray" <?php echo $analysis->lucky == 1 ? 'style="background: red"' : false;  ?> >  <?php echo $analysis->lucky == 1 ? '中' : '未';  ?> </span>
+            <span class="badge bg-gray" <?php echo $analysis->attributes['lucky'] == 1 ? 'style="background: red"' : false;  ?> >  <?php echo $analysis->attributes['lucky'] == 1 ? '中' : '未';  ?> </span>
         </td>
     </tr>
 <?php endforeach; ?>
