684142f295d82367fcb00172a0671b8469462138
夜云 <644362887@qq.com>
2018-11-22 18:03:48 +0800

    11选5

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -49,6 +49,7 @@ class SiteController extends Controller
 
     public function actionIndex()
     {
+        return $this->redirect('/home/new-code?type=1');
         $this->layout = 'main2';
         return $this->render('index');
     }
